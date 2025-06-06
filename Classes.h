7503e69e227682a0feb51d749d2247ee99de0397
Anthony Timoshin <95761331+anthonytimoshin@users.noreply.github.com>
2025-06-06 11:28:24 +0300

    Add files via upload
    06/06/2025

--- Classes.h
+++ Classes.h
@@ -25,19 +25,67 @@ public:
     virtual ~Point() = default; // Виртуальный деструктор
     void moveTo(QPainter* painter,
                 int newX, int newY);
-    void Hide(QPainter* painter);
-    void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+    virtual void Show(QPainter* painter);
     bool getVisibility() const;
     void setVisibility(bool visible);
 };
 
+//////////////////////////////////////
+/// \brief The Lamp class
+///
 class Lamp : public Point {
 public:
+    int id;
     Lamp(int x = 0, int y = 0, bool visible = true);
-    void moveTo(QPainter* painter,
-                int newX, int newY);
-     void Show(QPainter* painter);
-     void Hide(QPainter* painter);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
 };
+
+class BrokenLamp: public Lamp {
+public:
+    int id;
+    BrokenLamp(int x = 0, int y = 0, bool visible = true);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+};
+
+class MagicLamp:public Lamp
+{
+public:
+    int id;
+    MagicLamp(int x = 0, int y = 0, bool visible = true);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+
+};
+
+///////////////////////////////////////
+/// \brief The Fig class
+///
+class Fig : public Point {
+public:
+    Fig(int x = 0, int y = 0, bool visible = true);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+    int id;
+};
+
+class Energy : public Fig {
+public:
+    Energy(int x = 0, int y = 0, bool visible = true);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+    int id;
+};
+
+class Brick : public Fig {
+public:
+    Brick(int x = 0, int y = 0, bool visible = true);
+    virtual void Show(QPainter* painter);
+    virtual void Hide(QPainter* painter);
+    int id;
+};
+
 
 #endif // CLASSES_H
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,66 +6,135 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
     setFixedSize(800, 600);
     setFocusPolicy(Qt::StrongFocus);
-    point = new Point(100, 100, true); // Создаем Lamp, но храним как Point*
-    lamp = new Lamp(200,200,1);
+    point = new Point(100, 100, true);
+
+    // Инициализация ламп
+    brokenLamp = new BrokenLamp(400,250,1);
+    lamp = new Lamp(400,250,1);
+    magicLamp = new MagicLamp(400,250,1);
+
+    BufLamp[0] = brokenLamp;
+    BufLamp[1] = lamp;
+    BufLamp[2] = magicLamp;
+
+    // Инициализация препятствий
+    energy = new Energy(100,250,1);
+    brick = new Brick(600,275,1);
+
+    BufFig[0] = energy;
+    BufFig[1] = brick;
+
+    curLamp = BufLamp[1]; // сначала обычная лампа
 }
 
 MainWindow::~MainWindow() {
-    delete point; // Освобождаем память
-}
-
-void MainWindow::paintEvent(QPaintEvent *event) {
-    Q_UNUSED(event);
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    // Очистка холста
-    painter.fillRect(rect(), Qt::white);
-
-   // point->moveTo(&painter, newX, newY); // Убираем передачу painter
-
-    // Рисование объекта через указатель
-    // if (point->getVisibility()) {
-        // lamp->Show(&painter);
-    // } else {
-    //     point->Hide(&painter);
-    // }
-    int newX = lamp->getX();
-    int newY = lamp->getY();
-    lamp->moveTo(&painter,newX,newY);
-
+    delete point;
+    delete lamp;
+    delete magicLamp;
+    delete brokenLamp;
+    delete energy;
+    delete brick;
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-    // Очистка холста
-    painter.fillRect(rect(), Qt::white);
-
-
-    int newX = lamp->getX();
-    int newY = lamp->getY();
+    int newX = curLamp->getX();
+    int newY = curLamp->getY();
 
     switch (event->key()) {
-    case Qt::Key_W: // Вверх
+    case Qt::Key_W:
         newY -= step;
         break;
-    case Qt::Key_S: // Вниз
+    case Qt::Key_S:
         newY += step;
         break;
-    case Qt::Key_A: // Влево
+    case Qt::Key_A:
         newX -= step;
         break;
-    case Qt::Key_D: // Вправо
+    case Qt::Key_D:
         newX += step;
         break;
     default:
         QMainWindow::keyPressEvent(event);
         return;
     }
-  //  point->Show(&painter);
-    lamp->moveTo(&painter, newX, newY); // Убираем передачу painter
-    update(); // Перерисовка окна
+
+    int tempX = newX;
+    int tempY = newY;
+    bool collisionDetected = false;
+
+    for (int i = 0; i < 2; i++) {
+        curLamp->moveTo(nullptr, tempX, tempY);
+        if (CheckCollision(curLamp, BufFig[i])) {
+            collisionDetected = true;
+
+            // Матрица переходов: [текущая лампа][объект] -> новая лампа
+            // Индексы ламп: 0 - brokenLamp, 1 - lamp, 2 - magicLamp
+            // ИНдексы препятствий: 0 - energy, 1 - brick
+            Lamp* transitionMatrix[3][2] = {
+                /* brokenLamp */ {BufLamp[1], BufLamp[0]},  // energy -> lamp, brick -> brokenLamp
+                /* lamp */      {BufLamp[2], BufLamp[0]},  // energy -> magicLamp, brick -> brokenLamp
+                /* magicLamp */ {BufLamp[2], BufLamp[1]}   // energy -> magicLamp, brick -> lamp
+            };
+
+            // текущий тип лампы
+            int currentType = 0;
+            if (curLamp == BufLamp[1]) {
+                currentType = 1;
+            }
+            else if (curLamp == BufLamp[2]) {
+                currentType = 2;
+            }
+
+            // Определяем тип объекта (0 - energy, 1 - brick)
+            int objectType;
+            if (i == 0) {
+                objectType = 0; // energy
+            } else {
+                objectType = 1; // brick
+            }
+
+            // Получаем новую лампу из матрицы переходов
+            curLamp = transitionMatrix[currentType][objectType];
+
+            // Перемещаем лампу в безопасное место
+            tempX = 400;
+            tempY = 250;
+            curLamp->moveTo(nullptr, tempX, tempY);
+
+            break;
+        }
+    }
+
+    if (!collisionDetected) {
+        curLamp->moveTo(nullptr, newX, newY);
+    }
+
+    update();
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    painter.fillRect(rect(), Qt::white);
+    curLamp->moveTo(&painter, curLamp->getX(), curLamp->getY());
+
+    for (int i = 0; i < 2; i++) {
+        BufFig[i]->Show(&painter);
+    }
+}
+
+int MainWindow::CheckCollision(Lamp *l, Fig *f)
+{
+    int dx = std::abs(l->getX() - f->getX());
+    int dy = std::abs(l->getY() - f->getY());
+
+    if (dx < 50 && dy < 50) {
+        return 1; // есть столкновение
+    } else {
+        return 0; // нет столкновения
+    }
 }
